7c3da360ff7fb6728a10444018ce1ce27222f9e1
Vinay-kumary <113224482+Vinay-kumary@users.noreply.github.com>
2023-11-02 13:30:34 +0530

    Update 1.puzzle8.py

--- 1.puzzle8.py
+++ 1.puzzle8.py
@@ -1,95 +1,54 @@
 import heapq
-
-
-class PuzzleNode:
-    def __init__(self, state, parent=None, move=None, cost=0):
-        self.state = state
-        self.parent = parent
-        self.move = move
-        self.cost = cost
-        self.heuristic = self.calculate_heuristic()
-
-    def calculate_heuristic(self):
-        count = 0
-        for i in range(3):
-            for j in range(3):
-                if self.state[i][j] != goal_state[i][j]:
-                    count += 1
-        return count
-
-    def __lt__(self, other):
-        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
-
-
-def get_user_input():
-    print("Enter the initial configuration for the 8 puzzle problem row by row (separate each number by space):")
-    puzzle_input = []
-    for i in range(3):
-        while True:
-            row = input(f"Enter numbers for row {i + 1}: ").strip().split()
-            if len(row) == 3 and all(num.isdigit() for num in row):
-                puzzle_input.append([int(num) for num in row])
-                break
-            else:
-                print("Invalid input. Please enter 3 space-separated numbers.")
-    return puzzle_input
-
-
-def get_possible_moves(node):
+goal_state = (1, 2, 3, 8, 0, 4, 7, 6, 5)
+initial_state = (2, 8, 3, 1, 6, 4, 7, 0, 5)
+def manhattan_distance(state):
+    distance = 0
+    for i in range(9):
+        if state[i] != 0:
+            goal_row = (state[i] - 1) // 3
+            goal_col = (state[i] - 1) % 3
+            curr_row = i // 3
+            curr_col = i % 3
+            distance += abs(goal_row - curr_row) + abs(goal_col - curr_col)
+    return distance
+def generate_moves(state):
     moves = []
-    empty_space = None
-    for i in range(3):
-        for j in range(3):
-            if node.state[i][j] == 0:
-                empty_space = (i, j)
-                break
-
-    for move in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-        new_position = (empty_space[0] + move[0], empty_space[1] + move[1])
-        if 0 <= new_position[0] < 3 and 0 <= new_position[1] < 3:
-            new_state = [row.copy() for row in node.state]
-            new_state[empty_space[0]][empty_space[1]], new_state[new_position[0]][new_position[1]] = \
-                new_state[new_position[0]][new_position[1]], new_state[empty_space[0]][empty_space[1]]
-            moves.append(PuzzleNode(new_state, parent=node, move=move, cost=node.cost + 1))
+    empty_index = state.index(0)
+    if empty_index - 3 >= 0:
+        moves.append(empty_index - 3)
+    if empty_index + 3 < 9:
+        moves.append(empty_index + 3)
+    if empty_index % 3 != 0:
+        moves.append(empty_index - 1)
+    if (empty_index + 1) % 3 != 0:
+        moves.append(empty_index + 1)
     return moves
-
-
-def get_path(node):
-    path = []
-    while node.parent:
-        path.append(node.move)
-        node = node.parent
-    path.reverse()
-    return path
-
-
-def a_star_search(initial_state):
-    open_list = []
+def solve_puzzle(initial_state):
+    open_set = [(manhattan_distance(initial_state), initial_state)]
+    heapq.heapify(open_set)
     closed_set = set()
-    heapq.heappush(open_list, PuzzleNode(initial_state))
-    while open_list:
-        current_node = heapq.heappop(open_list)
-        if current_node.state == goal_state:
-            return get_path(current_node)
-        closed_set.add(tuple(map(tuple, current_node.state)))
-        for neighbor in get_possible_moves(current_node):
-            if tuple(map(tuple, neighbor.state)) not in closed_set:
-                heapq.heappush(open_list, neighbor)
-    return None
-
-
-if __name__ == '__main__':
-    goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-    initial_state = get_user_input()
-    print("Initial State:")
-    for row in initial_state:
-        print(row)
-
-    solution = a_star_search(initial_state)
-
-    if solution:
-        print("Moves to reach the goal state:")
-        for move in solution:
-            print(move)
-    else:
-        print("No solution found for the given input.")
+    parent_map = {initial_state: None}
+    while open_set:
+        _, current_state = heapq.heappop(open_set)
+        if current_state == goal_state:
+            path = []
+            while current_state is not None:
+                path.insert(0, current_state)
+                current_state = parent_map[current_state]
+            return path
+        closed_set.add(current_state)
+        moves = generate_moves(current_state)
+        for move in moves:
+            new_state = list(current_state)
+            new_state[current_state.index(0)], new_state[move] = new_state[move], new_state[current_state.index(0)]
+            new_state = tuple(new_state)
+            if new_state not in closed_set:
+                if (manhattan_distance(new_state), new_state) not in open_set:
+                    heapq.heappush(open_set, (manhattan_distance(new_state), new_state))
+                    parent_map[new_state] = current_state
+solution_path = solve_puzzle(initial_state)
+for step, state in enumerate(solution_path):
+    print(f"Step {step}:")
+    for i in range(0, 9, 3):
+        print(state[i:i+3])
+    print()
